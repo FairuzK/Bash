ce692e044961d1186c54e525b08954ffe1ddc600
FairuzK <kaweesafairuz@gmail.com>
2026-09-11 12:26:00 +0300

    Modifications to Script.sh file

--- Assignments/script.sh
+++ Assignments/script.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+echo "Enter filename Check:"
+read filename
+
+if [ -e "$filename" ]
+then
+    echo ""
+    echo "File '$filename' exists."
+    # Check if the file exists and is readable
+    if [ -r "$filename" ]
+    then
+    echo "File '$filename'is readable."
+    else    
+    echo "File '$filename'is not readable."
+    fi
+
+    # Check if the file is writable
+    if [-w "$filename" ]
+    then
+    echo "File '$filename' is writable."
+    else
+    echo "File '$filename' is not writable."
+    fi
+
+    # Check if the file is executable
+    if [ -x "$filename" ]
+    then
+    echo "File '$filename' is executable."
+    else
+    echo "File '$filename' is not executable."
+    fi
+else
+    echo "File '$filename' does not exist."
+fi
